bf29b2b78a7c2b48e88f75ae0d5b3b3c3899ff76
Raymond Fallon <git@niclic.com>
2020-01-22 10:56:12 -0500

    Hello World!

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/niclic/golang-book/pkg/chapter1"
+)
+
+func main () {
+	fmt.Println("This is the main golang-book command.")
+
+	chapter1.Hello()
+}
