435591764ecfda26ae86582d3be79f77e4d97447
Raymond Fallon <git@niclic.com>
2020-01-22 18:03:56 -0500

    Chapter 3 problems.

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"github.com/niclic/golang-book/pkg/chapter1"
+	"github.com/niclic/golang-book/pkg/chapter3"
 )
 
 func main() {
 	fmt.Println("This is the main golang-book command.")
 
 	chapter1.Hello()
+
+	fmt.Println(chapter3.Add(2, 3))
+	fmt.Println(chapter3.Multiply(32132, 42452))
 }
---
+++ pkg/chapter3/calculator.go
@@ -0,0 +1,11 @@
+package chapter3
+
+// Add: sums two integers
+func Add(x, y int) (sum int) {
+	return x + y
+}
+
+// Multiply: returns the product of two integers
+func Multiply(x, y int) (sum int) {
+	return x * y
+}
